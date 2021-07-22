91a31bdb6ec9d7b02c3f0fafd12d9b6a718734f8
Nic Crane <thisisnic@gmail.com>
2021-07-22 13:27:00 +0100

    Rename function files

---
+++ R/review_errors.R
@@ -0,0 +1,13 @@
+#' Review error messages
+#'
+#' @param path Path to package
+#'
+#' @export
+review_errors <- function(path){
+
+
+# - encourage use of "must", "should", etc
+# - readability
+
+
+}
--- R/review_vignettes.R
+++ R/review_vignettes.R
@@ -1,22 +1,125 @@
+
+#' Parse Vignette Results
+parse_vignette_results <- function(vignette_results){
+
+
+  cli_h3("Reading complexity")
+
+  rc <- lapply(vignette_results, function(x){
+    x$flesch_kincaid
+  })
+
+  iwalk(rc, ~warn_complexity(.y, round(.x,1)))
+
+  lengths <- lapply(vignette_results, function(x){
+    x$length
+  })
+
+  cli_h3("Length")
+  iwalk(lengths, ~warn_length(.y, .x))
+
+    # This needs entirely refactoring and adding back in as a feature
+
+
+  # pws <- lapply(vignette_results, function(x){
+  #   x$problem_words
+  # })
+  # pws_sections <- pws[lengths(pws) > 0]
+
+  # cli_h3("Problematic words")
+  # iwalk(pws_sections, ~warn_problem_section(.y, .x))
+
+
+}
+
+warn_length <- function(name, score, thresholds = c(3000, 2000)){
+
+  if (score > thresholds[1]) {
+    alert <- cli_alert_danger
+  } else if (score > thresholds[2]) {
+    alert <- cli_alert_warning
+  } else {
+    alert <- cli_alert_success
+  }
+
+  alert(paste(name, "has length:", score, "words."))
+
+}
+
+warn_complexity <- function(name, score, thresholds = c(30, 50)){
+
+  if (score < thresholds[1]) {
+    alert <- cli_alert_danger
+  } else if (score < thresholds[2]) {
+    alert <- cli_alert_warning
+  } else {
+    alert <- cli_alert_success
+  }
+
+  alert(paste(name, "has Flesch-Kincaid reading ability score:", score))
+
+}
+
+warn_problem_section <- function(name, content){
+  cli({
+    cli_alert_warning(
+    paste(
+      'Possible problem word ("simply", "naturally", "mere", "obvious", or "trivial") in vignette:',
+      name
+    )
+  )
+  lapply(content, function(problematic_section){
+
+    cli_alert(
+      paste(
+        "Detected in the following section:",
+        "\n",
+        problematic_section,
+        "\n"
+      )
+    )
+
+
+  })
+  })
+}
+
+#' Review vignettes
+#'
+#' @param path Path to package
+#'
+#' @export
+review_vignettes <- function(path){
+
+  vig_paths <- find_vignettes(path)
+  out <- lapply(vig_paths, analyse_vignette)
+  names(out) <- basename(vig_paths)
+  parse_vignette_results(out)
+
+}
+
+
+
 #' Analyse a single vignette
 #'
 #' @param vig_path Path to directory where vignette is
-analyse_vignette <- function(vig_path, problem_words){
+analyse_vignette <- function(vig_path){
 
   vig_sects <- parse_vignette(vig_path)
   cleaned_md <- lapply(vig_sects, clean_chunks)
   fk <- get_fk_score(cleaned_md)
   lengths <- get_length(cleaned_md)
-  pws <- detect_problem_words(cleaned_md, problem_words)
+  pws <- detect_problem_words(cleaned_md)
   list(flesch_kincaid = fk$overall, length = lengths$overall, problem_words = pws)
 }
 
-detect_problem_words <- function(md, problem_words){
+detect_problem_words <- function(md){
+  problem_words <- c("[Ss]imply", "[Nn]aturally", "[Mm]ere", "[Oo]bvious", "[Tt]rivial")
   problem_regex <- paste0(
       problem_words, collapse = "|"
     )
   out <- lapply(md, function(x){
-    stringr::str_detect(tolower(x), problem_regex)
+    stringr::str_detect(x, problem_regex)
   })
 
   md[out == TRUE]
