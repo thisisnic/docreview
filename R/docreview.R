3c9a360bc0bc5a69273579101bf0bd9127a28b73
Nic Crane <thisisnic@gmail.com>
2021-08-08 13:37:46 +0100

    Add functionality to count failures/warnings

--- R/docreview.R
+++ R/docreview.R
@@ -8,20 +8,55 @@ NULL
 #' @param error_on_failure Raise an error on any negative reviews
 #' @param doc_types Types of documentation to review
 #' @export
-package_review <- function(path = ".", error_on_failure = FALSE, doc_types = c("functions", "vignettes")){
-
+package_review <- function(path = ".", error_on_failure = FALSE,
+                           error_on_warning = FALSE,
+                           doc_types = c("functions", "vignettes"),
+                           thresholds = default_thresholds()) {
   cli_h1("docreview Results")
 
+  results <- list()
+
   if ("functions" %in% doc_types) {
-    func_results <- function_review(path)
-    function_results_parse(func_results, error_on_failure)
+    results$functions <- function_review(path, thresholds)
+    function_results_display(results$functions)
   }
 
   if ("vignettes" %in% doc_types) {
-    vig_results <- vignette_review(path)
-    vignette_results_parse(vig_results, error_on_failure)
+    results$vignettes <- vignette_review(path)
+    vignette_results_parse(results$vignettes)
   }
 
+  check_results(results, error_on_failure, error_on_warning)
+}
+
+#' Check results and raise error if necessary
+#'
+#' Inspired by testthat
+#'
+#' @param results List of docreview results
+#' @param error_on_failure Raise error on review failures?
+#' @param error_on_warning Raise error on review warnings?
+#'
+#' @keywords internal
+check_results <- function(results, error_on_failure, error_on_warning) {
+  total_failures <- sum(map_dbl(results, "failures"))
+  total_warnings <- sum(map_dbl(results, "warnings"))
+
+  if (error_on_warning && total_warnings > 0) {
+    rlang::abort(
+      paste("\nFailures found by docreview:", total_failures, "\nWarnings found by docreview:", total_warnings),
+      call. = FALSE
+    )
+  }
+
+  if (error_on_failure && total_failures > 0) {
+    rlang::abort(
+      paste("\nFailures found by docreview:", total_failures),
+      call. = FALSE
+    )
+  }
+
+  invisible(results)
 }
 
 #' Review functions
@@ -29,10 +64,40 @@ package_review <- function(path = ".", error_on_failure = FALSE, doc_types = c("
 #' @param path Path to package
 #'
 #' @export
-function_review <- function(path) {
+function_review <- function(path, thresholds = default_thresholds()) {
 
-  list("exports_without_examples" = get_exports_without_examples(path))
+  function_thresholds <- thresholds$functions
 
+  detailed_results <- list(
+    exports_examples = get_exports_without_examples(path)
+  )
+
+  comments <- function_get_comments(detailed_results, function_thresholds)
+
+  list(failures = comments$fail, warnings = comments$warn, details = detailed_results)
+}
+
+#' Count failures and warnings for function review
+#'
+#' @param results Results of function review
+#' @param thresholds List of thresholds that result in fails or warnings
+#' @keywords internal
+function_get_comments <- function(results, thresholds) {
+
+  comments <- list(fail = 0, warn = 0)
+
+  # Count failures and warnings for exports without examples
+  need_examples <- sum(results$exports_examples)
+
+  if (need_examples > 0) {
+    if (thresholds$exports_without_examples == "fail") {
+      comments$fail <- comments$fail + need_examples
+    } else if (thresholds$exports_without_examples == "warn") {
+      comments$warn <- comments$warn + need_examples
+    }
+  }
+
+  comments
 }
 
 #' Review vignettes
@@ -40,9 +105,56 @@ function_review <- function(path) {
 #' @param path Path to package
 #'
 #' @export
-vignette_review <- function(path) {
+vignette_review <- function(path, thresholds) {
+
+  vignette_thresholds <- thresholds$vignettes
+
   vig_paths <- find_vignettes(path)
-  out <- lapply(vig_paths, analyse_vignette)
-  names(out) <- basename(vig_paths)
-  out
+  detailed_results <- lapply(vig_paths, analyse_vignette)
+  names(detailed_results) <- basename(vig_paths)
+
+  comments <- vignettes_get_comments(detailed_results, vignette_thresholds)
+
+  list(failures = comments$fail, warnings = comments$warn, details = detailed_results)
+
+}
+
+vignettes_get_comments <- function(results, thresholds){
+
+  comments <- list(fail = 0, warn = 0)
+
+  # Count failures and warnings for Flesch Kincaid scores
+  fk_scores <- map(results, "flesch_kincaid")
+  fk_fails <- length(fk_scores[fk_scores < thresholds$fk$fail])
+  fk_warns <- length(fk_scores[fk_scores >= thresholds$fk$fail && fk_scores < thresholds$fk$warn])
+
+  # Count failures and warnings for lengths
+  length_scores <- map(results, "length")
+  length_fails <- length(length_scores[length_scores > thresholds$length$fail])
+  length_warns <- length(length_scores[length_scores <= thresholds$length$fail && length_scores > thresholds$length$warn])
+
+
+  comments$fail <- comments$fail + fk_fails + length_fails
+  comments$warn <- comments$warn + fk_warns + length_warns
+
+  comments
+
+}
+
+default_thresholds <- function(){
+  list(
+    functions = list(
+      exports_without_examples = "fail"
+    ),
+    vignettes = list(
+      fk = list(
+        fail = 30,
+        warn = 50
+      ),
+      length = list(
+        fail = 3000,
+        warn = 2000
+      )
+    )
+  )
 }
