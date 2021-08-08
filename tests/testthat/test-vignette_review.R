535e333d3856f1c475706d31cfbc3c1c890dea69
Nic Crane <thisisnic@gmail.com>
2021-08-08 17:53:21 +0100

    Update unit tests

---
+++ tests/testthat/test-vignette_review.R
@@ -0,0 +1,15 @@
+test_that("vignette_review works as expected", {
+
+  vr <- vignette_review("./testpkg/", )
+  expect_equal(vr$failures, 0)
+  expect_equal(vr$warnings, 2)
+
+  expect_named(vr$details, c("testpkg.Rmd", "testpkg2.Rmd"))
+
+  expect_equal(vr$details$testpkg.Rmd$flesch_kincaid, 36.09, tolerance = 0.1)
+  expect_equal(vr$details$testpkg.Rmd$length, 351)
+
+  expect_equal(vr$details$testpkg2.Rmd$flesch_kincaid, 33.7, tolerance = 0.1)
+  expect_equal(vr$details$testpkg2.Rmd$length, 170)
+
+})
