db839a0f09d891c090c20c89290d464128ab7446
Nic Crane <thisisnic@gmail.com>
2021-08-08 17:54:00 +0100

    Fix default arg in vignette_review

--- R/docreview.R
+++ R/docreview.R
@@ -105,7 +105,7 @@ function_get_comments <- function(results, thresholds) {
 #' @param path Path to package
 #'
 #' @export
-vignette_review <- function(path, thresholds = default_thresholds()) {
+vignette_review <- function(path, thresholds = default_thresholds()$vignettes) {
 
   vig_paths <- find_vignettes(path)
   detailed_results <- lapply(vig_paths, analyse_vignette)
